31e6b0acf7339622c6018750ef3c61bded90bceb
Jair Ferraz <jairbferraz@outlook.com>
2021-12-10 21:24:27 -0300

    Primeiro envio com CRUD da pessoa quase completo

---
+++ application/controllers/Pessoa.php
@@ -0,0 +1,70 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Pessoa
+ *
+ * @author jairb
+ */
+class Pessoa extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('Pessoa_model', 'pessoa'); //pessoa este é um apelido para o model
+    }
+
+    public function index() {
+        $lista['pessoas'] = $this->pessoa->listar();
+        $this->load->view('pessoaCadastro', $lista);
+    }
+
+    public function inserir() {
+        //nome no vetor deve ser o mesmo nome do campo na tabela
+        $dados['nome'] = $this->input->post('nome');
+        $dados['telefone'] = $this->input->post('telefone');
+        $dados['email'] = $this->input->post('email');
+        $dados['endereco'] = $this->input->post('endereco');
+        if ($this->input->post('tpPessoa') == 'Fisica') {
+            $dados['cpf'] = $this->input->post('cpf');
+            $dados['sexo'] = $this->input->post('sexo');
+        }else{
+            $dados['cnpj'] = $this->input->post('cnpj');
+            $dados['nomeFantasia'] = $this->input->post('nomeFantasia');
+        }
+        $result = $this->pessoa->inserir($dados);
+        if($result==true){
+            $this->session->set_flashdata('true','msg');
+            redirect('pessoa');
+        }else{
+            $this->session->set_flashdata('err','msg');
+            redirect('pessoa');
+        }
+    }
+    
+    public function excluir($id) {
+        $result = $this->pessoa->deletar($id);
+        if($result==true){
+            $this->session->set_flashdata('true','msg');
+            redirect('pessoa');
+        }else{
+            $this->session->set_flashdata('err','msg');
+            redirect('pessoa');
+        }
+    }
+    
+    public function editar($idPessoa) {
+        $data['pessoa'] = $this->pessoa->editar($idPessoa);
+        $this->load->view('pessoaEditar',$data);
+    }
+    
+    public function atualizar() {
+        
+    }
+
+}
---
+++ application/views/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Projeto</title>
+    </head>
+    <body>
+        <h1>Home</h1>
+        <p>Olá mundo!</p>
+        <a href="<?php echo base_url(); ?>pessoa">Cadastro Pessoa</a>
+    </body>
+</html>
